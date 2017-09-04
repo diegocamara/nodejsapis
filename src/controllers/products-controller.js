c04226e01b401a65a20ae29804e9afa020e0f381
diegocamara <diegoluizjava.java@gmail.com>
2017-09-03 22:19:27 -0300

    Inclusão do product repository

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -3,11 +3,11 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 const ItemValidator = require('./../validators/item-validator');
-
+const ProductRepository = require('../repositories/product-repository');
 
 exports.get = (req, res, next) => {
 
-    Product.find({ active: true }, 'title slug price').then(data => {
+    ProductRepository.get().then(data => {
         res.status(200).send(data);
     }).catch(error => {
         res.status(400).send(error);
@@ -16,7 +16,7 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-    Product.findOne({ slug: req.params.slug, active: true }, 'title slug description price').then(data => {
+    ProductRepository.getBySlug(req.params.slug).then(data => {
         res.status(200).send(data);
     }).catch(error => {
         res.status(400).send(error);
@@ -24,7 +24,7 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-    Product.findById(req.params.id).then(data => {
+    ProductRepository.getById(req.params.id).then(data => {
         res.status(200).send(data);
     }).catch(error => {
         res.status(400).send(error);
@@ -32,7 +32,7 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-    Product.find({ tags: req.params.tag }, 'title slug description price tags').then(data => {
+    ProductRepository.getByTag(req.params.tag).then(data => {
         res.status(200).send(data);
     }).catch(error => {
         res.status(400).send(error);
@@ -44,15 +44,13 @@ exports.post = (req, res, next) => {
     let itemValidator = new ItemValidator();
 
     itemValidator.hasMaxLen(req.body.title, 10, 'O título deve conter no máximo 10 caracteres.');
-    
-    if(!itemValidator.isValid()){
+
+    if (!itemValidator.isValid()) {
         res.status(400).send(itemValidator.errors());
         return;
     }
 
-    let product = new Product(req.body);
-
-    product.save().then(item => {
+    ProductRepository.create(req.body).then(item => {
         res.status(201).send({
             message: 'Produto cadastrado com sucesso!'
         });
@@ -67,13 +65,11 @@ exports.post = (req, res, next) => {
 
 exports.put = (req, res, next) => {
 
-    Product.findByIdAndUpdate(req.params.id, {
-        $set: {
-            title: req.body.title,
-            desciption: req.body.description,
-            price: req.body.price,
-            slug: req.body.slug
-        }
+    ProductRepository.update(req.params.id, {
+        title: req.body.title,
+        desciption: req.body.description,
+        price: req.body.price,
+        slug: req.body.slug
     }).then(item => {
 
         res.status(200).send({
@@ -94,7 +90,7 @@ exports.put = (req, res, next) => {
 
 exports.delete = (req, res, mext) => {
 
-    Product.findOneAndRemove(req.body.id).then(item => {
+    ProductRepository.delete(req.body.id).then(item => {
         res.status(200).send({
             message: 'Produto removido com sucesso!'
         });
